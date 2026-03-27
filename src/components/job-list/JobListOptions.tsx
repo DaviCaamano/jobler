bfc133569be8dff5a61db9e6bb610848eda426b0
DaviCaamano <davisantacaamano@gmail.com>
2026-03-27 12:52:51 -0400

    synchronized CSS between job list and filters

--- src/components/job-list/JobListOptions.tsx
+++ src/components/job-list/JobListOptions.tsx
@@ -1,5 +1,4 @@
 import '@components/job-list/JobListOptions.css';
-import { ClipboardX, ClipboardCheck } from 'lucide-react';
 
 interface JobListOptionsProps {
     locked: boolean;
@@ -8,16 +7,11 @@ export const JobListOptions = ({ locked = true }: JobListOptionsProps) => {
     return (
         <div className="job-list-options_container">
             <button
-                className="job-list-options_applied-button"
+                className="job-list-options_applied-button button-lighting"
                 onClick={() => {}}
                 disabled={locked}
             >
-                Applied
-                <ClipboardCheck className="job-list-options_next-button-icon" />
-            </button>
-            <button className="job-list-options_next-button" onClick={() => {}} disabled={locked}>
-                Skip
-                <ClipboardX className="job-list-options_next-button-icon" />
+                Start Hunt
             </button>
         </div>
     );
